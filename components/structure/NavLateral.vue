2eae1c6eb62546a3f52bb542e7002e2efdf065a4
Atyson Jaime <atysonjaime@gmail.com>
2022-10-05 16:35:44 -0300

    :sparkles: Adiciona navegação lateral e mobile

---
+++ components/structure/NavLateral.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+<!-- Navbar para mobile -->
+b-navbar(toggleable="lg" type="dark" fixed='top' v-if='isMobile')
+  b-navbar-brand(href='#') Atyson Jaime
+  b-navbar-toggle(target='nav-collapse')
+  b-collapse#nav-collapse(is-nav)
+    b-nav(vertical)
+      b-nav-item(v-for="item in itensMenu" :key="item.id" :to='item.id' exact exact-active-class="active") {{item.name}}
+
+<!-- Aside para Desktop -->
+aside.d-flex.flex-column(aria-label='' v-else)
+  #aside-photo.p-2
+    b-avatar.border-photo(src="~/assets/img/perfil.jpg" size='10rem' lazy)
+  #aside-nav
+    b-nav(vertical)
+      b-nav-item(v-for="item in itensMenu" :key="item.id" :to='item.id' exact exact-active-class="active") {{item.name}}
+</template>
+
+<script>
+export default {
+  name: 'NavegaçãoLateral',
+  components: {},
+  props: {
+    isMobile: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      itensMenu: [
+        { id: '#about', name: 'SOBRE' },
+        { id: '#experiencias', name: 'EXPERIÊNCIAS' },
+        { id: '#formacao', name: 'FORMAÇÃO' },
+        { id: '#habilidades', name: 'HABILIDADES' },
+        { id: '#portifolio', name: 'PORTIFÓLIO' },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.navbar {
+  background-color: #15283c;
+
+  .navbar-toggler {
+    color: rgba(255, 255, 255, 0.5);
+    &:hover,
+    &:focus {
+      color: rgba(255, 255, 255, 0.75) !important;
+    }
+  }
+  .nav-item {
+    .nav-link {
+      font-weight: 800;
+      letter-spacing: 0.05rem;
+      text-transform: uppercase;
+      color: rgba(255, 255, 255, 0.5);
+
+      &:hover,
+      &:focus {
+        color: rgba(255, 255, 255, 0.75) !important;
+      }
+    }
+  }
+}
+aside {
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+
+  #aside-photo {
+    .border-photo {
+      border: 0.5rem solid rgba(255, 255, 255, 0.2);
+      border-color: #214162;
+    }
+  }
+
+  #aside-nav {
+    .nav-item {
+      display: flex;
+      justify-content: center;
+
+      .nav-link {
+        font-weight: 800;
+        letter-spacing: 0.05rem;
+        text-transform: uppercase;
+        color: rgba(255, 255, 255, 0.5);
+
+        &:hover,
+        &:focus {
+          color: rgba(255, 255, 255, 0.75) !important;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,69 @@
-<template>
-  <Tutorial />
+<template lang="pug">
+  #main-layout
+    #inside
+      #sidebar
+        NavLateral(:isMobile='isMobile')
+      #content.container-fluid.p-0
+        Tutorial
 </template>
 
 <script>
+import NavLateral from '../components/structure/NavLateral.vue'
 export default {
   name: 'IndexPage',
+  components: {
+    NavLateral,
+  },
+  data() {
+    return {
+      isMobile: false,
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', this.onResize)
+    this.onResize()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.onResize)
+  },
+  methods: {
+    onResize() {
+      if (window.innerWidth < 768) {
+        this.isMobile = true
+      } else {
+        this.isMobile = false
+      }
+    },
+  },
 }
 </script>
+
+<style lang="scss">
+@import '../assets/scss/main';
+
+#main-layout {
+  #inside {
+    display: flex;
+    flex-direction: row;
+  }
+
+  #sidebar {
+    @include media('>=768px') {
+      width: 17rem;
+      position: fixed;
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
+      background-color: $dark-blue;
+      height: 100vh;
+    }
+  }
+
+  #content {
+    @include media('>=768px') {
+      padding-left: 17rem !important;
+    }
+    @include media('<640px') {
+      padding-top: 56px !important;
+    }
+  }
+}
+</style>
