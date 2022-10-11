6b3e09d2e3a4c60db56a5f69cf3876cdbdcfcc36
Atyson Jaime <atysonjaime@gmail.com>
2022-10-11 14:18:47 -0300

    :hammer: Ajuste nas meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,16 +2,68 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'AtysonJaime.github.io',
+    title: 'Portfólio - Atyson Jaime',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Portfólio do Atyson Jaime',
+      },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'portfolio atyson jaime, atyson, atyson jaime, portfolio',
+      },
+      {
+        hid: 'designer',
+        name: 'designer',
+        content: 'Atyson Jaime <atysonjaime@gmail.com>',
+      },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'Portfólio - Atyson Jaime',
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Portfólio do Atyson Jaime',
+      },
+      {
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://atysonjaime.github.io',
+      },
+      {
+        hid: 'og:site_name',
+        name: 'og:site_name',
+        content: 'Portfólio - Atyson Jaime',
+      },
+      {
+        hid: 'copyright',
+        name: 'copyright',
+        content: 'https://atysonjaime.github.io',
+      },
+      { hid: 'category', name: 'category', content: 'website' },
+      { hid: 'robots', name: 'robots', content: 'index,follow' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  loading: {
+    color: '#15283c',
+    continuous: true,
+    failedColor: '#a94442',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
